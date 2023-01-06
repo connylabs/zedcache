5dda779436905b17b10272885bfe1139cd9cd381
leonnicolas <leonloechner@gmx.de>
2023-01-06 15:53:10 +0100

    Restructure directories and packages
    Move all cache implementations under `./cache`.
    
    Signed-off-by: leonnicolas <leonloechner@gmx.de>

--- zedcache_test.go
+++ zedcache_test.go
@@ -16,7 +16,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
-	"github.com/connylabs/zedcache/go-cache"
+	"github.com/connylabs/zedcache/cache/go-cache"
 )
 
 func TestCheckPermission(t *testing.T) {
